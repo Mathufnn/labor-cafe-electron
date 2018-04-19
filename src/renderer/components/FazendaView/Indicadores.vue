2b3c5f29a36c795d3f80c06f3d396e7f9b9c594b
Matheus Henrique de Freitas <matheus.hrn@gmail.com>
2018-04-19 03:50:49 -0300

    + indicadores fazenda

--- src/renderer/components/FazendaView/Indicadores.vue
+++ src/renderer/components/FazendaView/Indicadores.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-title class="headline">
-      Indicadores
+      Indicadores da fazenda
     </v-card-title>
     <v-divider></v-divider>
     <v-layout>
@@ -26,11 +26,33 @@ export default {
     return {
       indicadores: {
         rendabruta: { text: 'RENDA BRUTA', status: 1, value: 1124.56 },
-        coe: { text: 'CUSTO OPERACIONAL EFETIVO', status: 3, value: 123.0 },
-        cot: { text: 'CUSTO OPERACIONAL TOTAL', status: 4, value: 3124.56 },
-        ct: { text: 'CUSTO TOTAL', status: 3, value: 'R$ ' + 21000 },
+        coe: { text: 'CUSTO OPERACIONAL EFETIVO (COE)', status: 3, value: 123.0 },
+        cot: { text: 'CUSTO OPERACIONAL TOTAL (COT)', status: 4, value: 3124.56 },
+        ct: { text: 'CUSTO TOTAL (CT)', status: 3, value: 'R$ ' + 21000 },
         pcv: { text: 'PREÇO MÉDIO DE VENDA', status: 2, value: 500 },
-        producao: { text: 'PRODUÇÃO', status: 1, value: 'R$ ' + 21000 }
+        producao: { text: 'PRODUÇÃO', status: 1, value: 'R$ ' + 21000 },
+        aplantada: { text: 'ÁREA PLANTADA', status: 1, value: 1254 + ' ha' },
+        ppaplantada: { text: 'PRODUÇÃO P/ ÁREA PLANTADA', status: 1, value: 12  },
+        coeap: { text: 'COE POR ÁREA PLANTADA', status: 4, value: 345  },
+        coeu: { text: 'COE POR UNIDADE', status: 2, value: 234  },
+        cotap: { text: 'COT POR ÁREA PLANTADA', status: 2, value: 532  },
+        cotu: { text: 'COT POR UNIDADE', status: 4, value: 56.45  },
+        ctap: { text: 'CT POR ÁREA PLANTADA', status: 3, value: 125.4  },
+        ctu: { text: 'CT POR UNIDADE', status: 3, value: 432  },
+        mb: { text: 'MARGEM BRUTA', status: 4, value: 45  },
+        mbap: { text: 'MARGEM BRUTA POR ÁREA PLANTADA', status: 3, value: 152.21  },
+        mbu: { text: 'MARGEM BRUTA POR UNIDADE', status: 3, value: 255.61  },
+        ml: { text: 'MARGEM LÍQUIDA', status: 3, value: 25.56  },
+        mlap: { text: 'MARGEM LÍQUIDA POR ÁREA PLANTADA', status: 2, value: 812.1  },
+        mlu: { text: 'MARGEM LÍQUIDA POR UNIDADE', status: 4, value: 124.13  },
+        lucro: { text: 'LUCRO', status: 2, value: 975.13  },
+        lucroap: { text: 'LUCRO POR ÁREA PLANTADA', status: 3, value: 64.1  },
+        lucrou: { text: 'LUCRO POR UNIDADE', status: 4, value: 5172.1  },
+        trcst: { text: 'TAXA DE REMUNERAÇÃO DO CAPITAL SEM TERRA', status: 2, value: 1245  },
+        trcct: { text: 'TAXA DE REMUNERAÇÃO DO CAPITAL COM TERRA', status: 1, value: 12  },
+        bencusto: { text: 'RELAÇÃO BENEFÍCIO/CUSTO', status: 1, value: 48  },
+        capitalest: { text: 'CAPITAL EMPATADO SEM TERRA', status: 3, value: 8644.41  },
+        capitalct: { text: 'CAPITAL EMPATADO COM TERRA', status: 3, value: 2537  }
       }
     }
   }

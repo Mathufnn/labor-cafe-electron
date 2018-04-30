d6402f35ee208ab782bdb1c8c81d847eb8e33842
Matheus Henrique <matheus.hrn@gmail.com>
2018-04-29 21:35:30 -0300

    Consertado key warning

--- src/renderer/components/FazendaView/Indicadores.vue
+++ src/renderer/components/FazendaView/Indicadores.vue
@@ -7,7 +7,7 @@
     <v-layout>
       <v-container row wrap justify-space-around>
         <v-layout row wrap class="text-xs-center">
-          <v-flex xs4 v-for="i in indicadores" v-bind:key="i.value">
+          <v-flex xs4 v-for="i in indicadores" v-bind:key="i.text">
             <v-card :class="'status' + i.status">
               <b>{{i.text}}</b><br /><span class="indicator">{{i.value}}</span>
             </v-card>

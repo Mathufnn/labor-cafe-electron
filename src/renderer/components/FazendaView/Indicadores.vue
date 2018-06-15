e1cf68d0260a50cb6a4ab3efba1d015bf1c60cf6
Matheus Henrique <matheus.hrn@gmail.com>
2018-06-15 19:46:03 -0300

    formatacao, unidade e arrendodamento

--- src/renderer/components/FazendaView/Indicadores.vue
+++ src/renderer/components/FazendaView/Indicadores.vue
@@ -9,7 +9,7 @@
         <v-layout row wrap class="text-xs-center">
           <v-flex xs4 v-for="i in indicadores" v-bind:key="i.text">
             <v-card :class="'status' + i.status">
-              <b>{{i.text}}</b><br /><span class="indicator">{{i.value}} <span class="caption">{{i.unidade}}</span></span>
+              <b>{{i.text}}</b><br /><span class="indicator">{{formatN(i.value)}}</span> <span class="unidade"><b>{{i.unidade}}</b></span>
             </v-card>
           </v-flex>
         </v-layout>
@@ -24,41 +24,46 @@ export default {
     return {
       indicadores: {
         rendabruta: { text: 'RENDA BRUTA', status: 3, value: 0, unidade: 'R$/Ano' },
-        coe: { text: 'CUSTO OPERACIONAL EFETIVO (COE)',  status: 3, value: 0 },
-        cot: { text: 'CUSTO OPERACIONAL TOTAL (COT)',status: 3, value: 0 },
-        ct: { text: 'CUSTO TOTAL (CT)', status: 3, value: 0},
-        pcv: { text: 'PREÇO MÉDIO DE VENDA', status: 3, value: 0 },
-        producao: { text: 'PRODUÇÃO',status: 3, value: 0 },
-        aplantada: { text: 'ÁREA PLANTADA', status: 3, value: 0 },
-        ppaplantada: { text: 'PRODUÇÃO PARA ÁREA PLANTADA', status: 3, value: 0  },
-        coeap: { text: 'COE POR ÁREA PLANTADA', status: 3, value: 0 },
-        coeu: { text: 'COE POR UNIDADE', status: 3, value: 0  },
-        cotap: { text: 'COT POR ÁREA PLANTADA', status: 3, value: 0 },
-        cotu: { text: 'COT POR UNIDADE', status: 3, value: 0 },
-        ctap: { text: 'CT POR ÁREA PLANTADA', status: 3, value: 0 },
-        ctu: { text: 'CT POR UNIDADE', status: 3, value: 0 },
-        mb: { text: 'MARGEM BRUTA', status: 3, value: 0  },
-        mbap: { text: 'MARGEM BRUTA POR ÁREA PLANTADA', status: 3, value: 0  },
-        mbu: { text: 'MARGEM BRUTA POR UNIDADE', status: 3, value: 0 },
-        ml: { text: 'MARGEM LÍQUIDA',status: 3, value: 0  },
-        mlap: { text: 'MARGEM LÍQUIDA POR ÁREA PLANTADA', status: 3, value: 0},
-        mlu: { text: 'MARGEM LÍQUIDA POR UNIDADE',status: 3, value: 0  },
-        lucro: { text: 'LUCRO',status: 3, value: 0},
-        lucroap: { text: 'LUCRO POR ÁREA PLANTADA', status: 3, value: 0},
-        lucrou: { text: 'LUCRO POR UNIDADE', status: 3, value: 0  },
-        trcst: { text: 'TAXA DE REMUNERAÇÃO DO CAPITAL SEM TERRA',status: 3, value: 0  },
-        trcct: { text: 'TAXA DE REMUNERAÇÃO DO CAPITAL COM TERRA', status: 3, value: 0  },
-        bencusto: { text: 'RELAÇÃO BENEFÍCIO/CUSTO',status: 3, value: 0 },
-        capitalest: { text: 'CAPITAL EMPATADO SEM TERRA', status: 3, value: 0 },
-        capitalct: { text: 'CAPITAL EMPATADO COM TERRA', status: 3, value: 0  },
-        taxagiro: { text: 'TAXA DE GIRO', status: 3, value: 0 },
-        lucrativ: { text: 'LUCRATIVIDADE', status: 3, value: 0 }
+        coe: { text: 'CUSTO OPERACIONAL EFETIVO (COE)',  status: 3, value: 0, unidade: 'R$/Ano' },
+        cot: { text: 'CUSTO OPERACIONAL TOTAL (COT)',status: 3, value: 0, unidade: 'R$/Ano' },
+        ct: { text: 'CUSTO TOTAL (CT)', status: 3, value: 0, unidade: 'R$/Ano'},
+        pcv: { text: 'PREÇO MÉDIO DE VENDA', status: 3, value: 0, unidade: 'R$/Sc' },
+        producao: { text: 'PRODUÇÃO',status: 3, value: 0, unidade: 'Sacas' },
+        aplantada: { text: 'ÁREA PLANTADA', status: 3, value: 0, unidade: 'Ha' },
+        ppaplantada: { text: 'PRODUÇÃO POR ÁREA PLANTADA', status: 3, value: 0, unidade: 'Und/Ha'  },
+        coeap: { text: 'COE POR ÁREA PLANTADA', status: 3, value: 0, unidade: 'R$/Ha' },
+        coeu: { text: 'COE POR UNIDADE', status: 3, value: 0, unidade: 'R$/Sc'  },
+        cotap: { text: 'COT POR ÁREA PLANTADA', status: 3, value: 0, unidade: 'R$/Ha' },
+        cotu: { text: 'COT POR UNIDADE', status: 3, value: 0, unidade: 'R$/Sc' },
+        ctap: { text: 'CT POR ÁREA PLANTADA', status: 3, value: 0, unidade: 'R$/Ha' },
+        ctu: { text: 'CT POR UNIDADE', status: 3, value: 0, unidade: 'R$/Sc' },
+        mb: { text: 'MARGEM BRUTA', status: 3, value: 0, unidade: 'R$/Ano'  },
+        mbap: { text: 'MARGEM BRUTA POR ÁREA PLANTADA', status: 3, value: 0, unidade: 'R$/Ha'  },
+        mbu: { text: 'MARGEM BRUTA POR UNIDADE', status: 3, value: 0, unidade: 'R$/Sc' },
+        ml: { text: 'MARGEM LÍQUIDA',status: 3, value: 0, unidade: 'R$/Ano'  },
+        mlap: { text: 'MARGEM LÍQUIDA POR ÁREA PLANTADA', status: 3, value: 0, unidade: 'R$/Ha'},
+        mlu: { text: 'MARGEM LÍQUIDA POR UNIDADE',status: 3, value: 0, unidade: 'R$/Sc'  },
+        lucro: { text: 'LUCRO',status: 3, value: 0, unidade: 'R$/Ano'},
+        lucroap: { text: 'LUCRO POR ÁREA PLANTADA', status: 3, value: 0, unidade: 'R$/Ha'},
+        lucrou: { text: 'LUCRO POR UNIDADE', status: 3, value: 0, unidade: 'R$/Sc'  },
+        trcst: { text: 'TAXA DE REMUNERAÇÃO DO CAPITAL SEM TERRA',status: 3, value: 0, unidade: '%'  },
+        trcct: { text: 'TAXA DE REMUNERAÇÃO DO CAPITAL COM TERRA', status: 3, value: 0, unidade: '%'  },
+        bencusto: { text: 'RELAÇÃO BENEFÍCIO/CUSTO',status: 3, value: 0, unidade: 'R$' },
+        capitalest: { text: 'CAPITAL EMPATADO SEM TERRA', status: 3, value: 0, unidade: 'R$/Sc' },
+        capitalct: { text: 'CAPITAL EMPATADO COM TERRA', status: 3, value: 0, unidade: 'R$/Sc'  },
+        taxagiro: { text: 'TAXA DE GIRO', status: 3, value: 0, unidade: '%a.a' },
+        lucrativ: { text: 'LUCRATIVIDADE', status: 3, value: 0, unidade: '%a.a' }
       }
     }
   },
   props: {
     fid: {
       default: '-1'
+    }
+  },
+  methods: {
+    formatN(vr){
+      return vr.toLocaleString('pt-BR');
     }
   },
   mounted: function() {
@@ -69,7 +74,81 @@ export default {
           this.$backend.getSafraTalhao(safraObj.id, all_talhao => {
             if(all_talhao != null)
             all_talhao.forEach(talhaoObj => {
-              this.indicadores.rendabruta.value += talhaoObj.ProdTotal * safraObj.PrecoVenda;
+              //renda bruta
+              this.indicadores.rendabruta.value += Math.floor(talhaoObj.ProdTotal * safraObj.PrecoVenda);
+              //coe
+              this.indicadores.coe.value += Math.floor(talhaoObj.ArrendamentoTerras + talhaoObj.AluguelMaquinas + talhaoObj.Combustivel + talhaoObj.ManutencaoBenf + talhaoObj.ManutencaoMaq + talhaoObj.EnergiaEletrica + talhaoObj.Frete + talhaoObj.Impostos + talhaoObj.MaoObraContratada + talhaoObj.MaoObraFixa + talhaoObj.Despesas + talhaoObj.Assistencia + talhaoObj.Certificacao + talhaoObj.AnaliseSolo + talhaoObj.AnaliseFoliar + talhaoObj.EPi + talhaoObj.Acidos + talhaoObj.Adubos + talhaoObj.Acaricida  + talhaoObj.Bactericida + talhaoObj.Espalhante + talhaoObj.Fungicida + talhaoObj.Inseticida + talhaoObj.Nematicida + talhaoObj.OleoMineral + talhaoObj.Herbicida + talhaoObj.Hormonios + talhaoObj.Maturadores + talhaoObj.MaterialColheita + talhaoObj.Armazenamento + talhaoObj.Beneficios + talhaoObj.GasLenhaCarvao + talhaoObj.PosColheita + talhaoObj.Rebeneficio + talhaoObj.Saco + talhaoObj.Correntagem);
+
+              //cot
+              this.indicadores.cot.value += Math.floor(this.indicadores.coe.value + talhaoObj.MaoObraF) /* + CAPITALESTOQUE DEPRECIACAO */;
+
+              //ct
+              this.indicadores.ct.value += Math.floor(this.indicadores.cot.value) /* + CAPITALESTOQUE REMUNERACAO CAPITAL */;
+
+              //pcv
+              this.indicadores.pcv.value += Math.floor(safraObj.PrecoVenda);
+
+              //producao
+              this.indicadores.producao.value += Math.floor(talhaoObj.ProdTotal);
+
+              //aplantada
+              this.indicadores.aplantada.value += Math.floor(safraObj.AreaProducao);
+
+              //ppaplantada
+              this.indicadores.ppaplantada.value += Math.floor(this.indicadores.producao.value / this.indicadores.aplantada.value);
+
+              //coeap
+              this.indicadores.coeap.value += Math.floor(this.indicadores.coe.value / this.indicadores.aplantada.value);
+
+              //coeu
+              this.indicadores.coeu.value +=Math.floor( this.indicadores.coe.value / this.indicadores.producao.value);
+
+              //cotap
+              this.indicadores.cotap.value += Math.floor(this.indicadores.cot.value / this.indicadores.aplantada.value);
+
+              //cotu
+              this.indicadores.cotu.value += Math.floor(this.indicadores.cot.value / this.indicadores.producao.value);
+
+              //ctap
+              this.indicadores.ctap.value += Math.floor(this.indicadores.ct.value / this.indicadores.aplantada.value);
+
+              //ctu
+              this.indicadores.ctu.value += Math.floor(this.indicadores.ct.value / this.indicadores.producao.value);
+
+              //mb
+              this.indicadores.mb.value += Math.floor(this.indicadores.rendabruta.value - this.indicadores.coe.value);
+
+              //mbap
+              this.indicadores.mbap.value += Math.floor(this.indicadores.mb.value / this.indicadores.aplantada.value);
+
+
+              this.indicadores.mbu.value += Math.floor(this.indicadores.mb.value / this.indicadores.producao.value);
+
+              this.indicadores.ml.value += Math.floor(this.indicadores.rendabruta.value - this.indicadores.cot.value);
+
+              this.indicadores.mlap.value += Math.floor(this.indicadores.ml.value / this.indicadores.aplantada.value);
+
+              this.indicadores.mlu.value +=Math.floor( this.indicadores.ml.value / this.indicadores.producao.value);
+
+              this.indicadores.lucro.value += Math.floor(this.indicadores.rendabruta.value - this.indicadores.ct.value);
+
+              this.indicadores.lucroap.value += Math.floor(this.indicadores.lucro.value / this.indicadores.aplantada.value);
+
+              this.indicadores.lucrou.value += Math.floor(this.indicadores.lucro.value / this.indicadores.producao.value);
+
+              this.indicadores.trcst.value +=Math.floor( this.indicadores.ml.value); /* / ESTOQC EM LAVOURAS +   BENFEITORIAS */ ;
+
+              this.indicadores.trcct.value +=Math.floor( this.indicadores.ml.value); /* / ESTOQC EM LAVOURAS +   BENFEITORIAS  +  estoque capital em terra */;
+
+              this.indicadores.vbencusto.value += Math.floor(this.indicadores.rendabruta.value / this.indicadores.ct.value);
+
+              this.indicadores.capitalest.value +=  /* ESTOQC EM LAVOURAS +   BENFEITORIAS  / */  Math.floor(this.indicadores.producao.value);
+
+              this.indicadores.capitalct.value +=   /* ESTOQC EM LAVOURAS +   BENFEITORIAS + estoque de capital em terra / */ Math.floor(this.indicadores.producao.value);
+
+              this.indicadores.taxagiro.value += Math.floor((this.indicadores.rendabruta.value / this.indicadores.producao.value) / this.indicadores.capitalct.value);
+
+              this.indicadores.lucrativ.value += Math.floor(this.indicadores.mlu.value / this.indicadores.capitalct.value);
 
             });
           });
@@ -83,6 +162,11 @@ export default {
 <style scoped>
 .indicator {
   font-size:41px;
+}
+
+.unidade {
+  font-size:15px;
+  color:#B5B5B5;
 }
 
 .status1 {

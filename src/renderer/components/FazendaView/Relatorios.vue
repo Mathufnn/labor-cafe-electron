ab25d0bf243ff41e5edc4acac209726d735beead
Gabriel M Silva <gabriel.silva@nobugs.com.br>
2018-06-19 11:41:40 -0300

    inicio do gerador de pdf

--- src/renderer/components/FazendaView/Relatorios.vue
+++ src/renderer/components/FazendaView/Relatorios.vue
@@ -6,7 +6,6 @@
           <v-btn small color="info" slot="activator" v-on:click="preview" ><v-icon center dark>description</v-icon>  GERAR RELATÓRIO</v-btn>
           <span>Gerar Relatório PDF</span>
           <v-card v-if="load">
-            <img src="../../assets/preview.png" width="600px"/> <!-- selecionar outra imagem -->
             <v-card-actions>
               <v-spacer></v-spacer>
 
@@ -25,18 +24,73 @@
 
 <script>
 import { BrowserWindow, remote } from 'electron'
+import indicadores from './Indicadores.vue'
 
 export default {
   data: () => ({
     load: false,
 
   }),
+  props: {
+    fid: {
+      default: '-1'
+    }
+  },
   methods: {
     preview: function() {
-      //inserir código para gerar relatorio em pdf
+      var pdf = require('pdfkit');
+      var fs = require('fs');
+
+      var myDoc = new pdf;
+
+      myDoc.pipe(fs.createWriteStream('Relatório.pdf'));
+
+      myDoc.font('Times-Roman')
+      myDoc.fontSize(40)
+      myDoc.text('Relatório da Safra ')
+           .fontSize(25)
+           .text('Indicadores: ')
+           .fontSize(18)
+           .text('RENDA BRUTA: ' + 0)
+           .text('CUSTO OPERACIONAL EFETIVO (COE): ' + 0)
+           .text('CUSTO OPERACIONAL TOTAL (COT): ' + 0)
+           .text('CUSTO TOTAL (CT): ' + 0)
+           .text('PREÇO MÉDIO DE VENDA: ' + 0)
+           .text('PRODUÇÃO: ' + 0)
+           .text('ÁREA PLANTADA: ' + 0)
+           .text('PRODUÇÃO POR ÁREA PLANTADA: ' + 0)
+           .text('COE POR ÁREA PLANTADA: ' + 0)
+           .text('COE POR UNIDADE: ' + 0)
+           .text('COT POR ÁREA PLANTADA: ' + 0)
+           .text('COT POR UNIDADE: ' + 0)
+           .text('CT POR ÁREA PLANTADA: ' + 0)
+           .text('CT POR UNIDADE: ' + 0)
+           .text('MARGEM BRUTA: ' + 0)
+           .text('MARGEM BRUTA POR ÁREA PLANTADA: ' + 0)
+           .text('MARGEM BRUTA POR UNIDADE: ' + 0)
+           .text('MARGEM LÍQUIDA: ' + 0)
+           .text('MARGEM LÍQUIDA POR ÁREA PLANTADA: ' + 0)
+           .text('MARGEM LÍQUIDA POR UNIDADE: ' + 0)
+           .text('LUCRO: ' + 0)
+           .text('LUCRO POR ÁREA PLANTADA: ' + 0)
+           .text('LUCRO POR UNIDADE: ' + 0)
+           .text('TAXA DE REMUNERAÇÃO DO CAPITAL SEM TERRA: ' + 0)
+           .text('TAXA DE REMUNERAÇÃO DO CAPITAL COM TERRA: ' + 0)
+           .text('RELAÇÃO BENEFÍCIO/CUSTO: ' + 0)
+           .text('CAPITAL EMPATADO SEM TERRA: ' + 0)
+           .text('CAPITAL EMPATADO COM TERRA: ' + 0)
+           .text('TAXA DE GIRO: ' + 0)
+           .text('LUCRATIVIDADE: ' + 0)
+
+
+
+
+      myDoc.end();
+      //console.log(this.indicadores.rendabruta.value);
       this.load = !this.load;
     },
   }
+
  }
 </script>
 

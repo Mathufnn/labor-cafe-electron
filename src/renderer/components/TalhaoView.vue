2b927e3b35d2036de5752d06eee400ddcf8c6e7f
Matheus Henrique <matheus.hrn@gmail.com>
2018-11-16 02:47:23 -0200

    export despesa

--- src/renderer/components/TalhaoView.vue
+++ src/renderer/components/TalhaoView.vue
@@ -31,6 +31,10 @@
               <v-btn small color="secondary" slot="activator" v-on:click="showElementos"><v-icon center dark>menu</v-icon></v-btn>
               <span>Mostrar elementos de despesa</span>
             </v-tooltip>
+            <v-tooltip top>
+              <v-btn icon slot="activator" @click="export_dialog = true" color="primary"> <v-icon>publish</v-icon></v-btn>
+              <span>Exportar dados</span>
+            </v-tooltip>
           </v-card-title>
           <v-container fluid v-if="view">
             <h2>ELEMENTOS DE DESPESA:</h2><br />
@@ -165,16 +169,78 @@
 
 
     </v-layout>
+    <v-dialog max-width="400" v-model="export_dialog" hide-overlay="true">
+      <v-card>
+        <v-card-text>
+          <b>Selecione o formato para qual deseja exportar esses dados:</b><br /><br />
+          <v-layout align-center justify-space-between row fill-height>
+            <v-flex xs6 class="text-xs-center">
+              <v-btn color="primary" big  @click="exportdata(1)">PDF</v-btn>
+            </v-flex>
+            <v-flex  xs6 class="text-xs-center">
+              <v-btn color="primary" big @click="exportdata(2)">Excel</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+        <v-btn color="green darken-1" flat="flat" @click="export_dialog = false">CANCELAR</v-btn>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
 import Indicadores from './TalhaoView/Indicadores'
+import { remote, ipcRenderer } from 'electron'
+import fs from 'fs'
+import path from 'path'
+import XLSX from 'XLSX'
 
 export default {
   name: 'TalhaoView',
   components: { Indicadores },
   data: () => ({
+    dados_comp: {
+      venda_subp:  { text: 'VENDA DE SUB PRODUTO', dado: () => this.venda_subp, unidade: 'R$/safra' },
+      mao_familiar:  { text: 'MÃO DE OBRA FAMILIAR ', dado: () => this.mao_familiar, unidade: 'R$/safra' },
+      arrend_terra:  { text: 'ARRENDAMENTO DE TERRAS ', dado: () => this.arrend_terra, unidade: 'R$/safra' },
+      aluguel_maq:  { text: 'ALUGUEL DE MÁQUINAS ', dado: () => this.aluguel_maq, unidade: 'R$/safra' },
+      combustivel:  { text: 'COMBUSTÍVEL ', dado: () => this.combustivel, unidade: 'R$/safra' },
+      manu_benf:  { text: 'MANUTENÇÃO DE MÁQUINAS ', dado: () => this.manu_maq, unidade: 'R$/safra' },
+      manu_maq:  { text: 'MANUTENÇÃO DE BENFEITORIAS ', dado: () => this.manu_benf, unidade: 'R$/safra' },
+      energia:  { text: 'ENERGIA ', dado: () => this.energia, unidade: 'R$/safra' },
+      frete:  { text: 'FRETE E DESLOCAMENTO ', dado: () => this.frete, unidade: 'R$/safra' },
+      impostos:  { text: 'IMPOSTOS E TAXAS ', dado: () => this.impostos, unidade: 'R$/safra' },
+      mao_contratada:  { text: 'MÃO DE OBRA CONTRATADA ', dado: () => this.mao_contratada, unidade: 'R$/safra' },
+      mao_fixa:  { text: 'MÃO DE OBRA FIXA ', dado: () => this.mao_fixa, unidade: 'R$/safra' },
+      despesas_gerais:  { text: 'DESPESAS GERAIS ', dado: () => this.despesas_gerais, unidade: 'R$/safra' },
+      ass_tec:  { text: 'ASSISTÊNCIA TÉCNICA ', dado: () => this.ass_tec, unidade: 'R$/safra' },
+      certificacao:  { text: 'CERTIFICAÇÃO ', dado: () => this.certificacao, unidade: 'R$/safra' },
+      analise_solo:  { text: 'ANÁLISE DE SOLO', dado: () => this.analise_solo, unidade: 'R$/safra' },
+      analise_foliar:  { text: 'ANÁLISE FOLIAR ', dado: () => this.analise_foliar, unidade: 'R$/safra' },
+      epi:  { text: 'EPI ', dado: () => this.epi, unidade: 'R$/safra' },
+      fertilizantes:  { text: 'FERTILIZANTES DE SOLO ', dado: () => this.fertilizantes, unidade: 'R$/safra' },
+      acidos:  { text: 'ÁCIDOS ', dado: () => this.acidos, unidade: 'R$/safra' },
+      adubos: { text: 'ADUBOS ', dado: () => this.adubos, unidade: 'R$/safra' },
+      acaricida:  { text: 'ACARICIDAS ', dado: () => this.acaricida, unidade: 'R$/safra' },
+      bactericida:  { text: 'BACTERICIDAS ', dado: () => this.bactericida, unidade: 'R$/safra' },
+      espalhante:  { text: 'ESPALHANTE ADESIVO', dado: () => this.espalhante, unidade: 'R$/safra' },
+      inseticida:  { text: 'INSETICIDA ', dado: () => this.inseticida, unidade: 'R$/safra' },
+      fungicida:  { text: 'FUNGICIDA ', dado: () => this.fungicida, unidade: 'R$/safra' },
+      nematicida:  { text: 'NEMATICIDA ', dado: () => this.nematicida, unidade: 'R$/safra' },
+      oleo_mineral:  { text: 'ÓLEO MINERAL ', dado: () => this.oleo_mineral, unidade: 'R$/safra' },
+      herbicida:  { text: 'HERBICIDA ', dado: () => this.herbicida, unidade: 'R$/safra' },
+      hormonios:  { text: 'HORMÔNIOS ', dado: () => this.hormonios, unidade: 'R$/safra' },
+      maturadores:  { text: 'MATURADORES ',dado: () => this.maturadores, unidade: 'R$/safra' },
+      material_colheita:  { text: 'MATERIAL COLHEITA ', dado: () => this.material_colheita, unidade: 'R$/safra' },
+      armazenamento:  { text: 'ARMAZENAMENTO ', dado: () => this.armazenamento, unidade: 'R$/safra' },
+      beneficio:  { text: 'BENEFÍCIO ', dado: () => this.beneficio, unidade: 'R$/safra' },
+      gas_lenha:  { text: 'GÁS/LENHA/CARVÃO ', dado: () => this.gas_lenha, unidade: 'R$/safra' },
+      material_pos_colheita:  { text: 'MATERIAIS PÓS COLHEITA ', dado: () => this.materiais_pos_colheita, unidade: 'R$/safra' },
+      rebeneficio:  { text: 'REBENEFÍCIO ', dado: () => this.rebeneficio, unidade: 'R$/safra' },
+      embalagens:  { text: 'SACOS/EMBALAGENS ', dado: () => this.embalagens, unidade: 'R$/safra' },
+      corretagem:  { text: 'CORRETAGEM ', dado: () => this.corretagem, unidade: 'R$/safra' }
+    },
+    export_dialog: false,
     talhao_ident: '',
     safra_ident: '',
     fazenda_ident: '',
@@ -229,10 +295,96 @@ export default {
     }
   },
   methods: {
+    exportdata(type){
+        //=============================================================  csv
+        if(type==2){
+          remote.dialog.showSaveDialog({title: 'Selecione local para salvar o arquivo excel',filters:[{name: 'Arquivo XLSX', extensions: ['xlsx']}]}, (filename) => {
+            if(typeof filename == 'undefined') return;
+            //this.loading = true;
+            let data = [];
+            let count = 1;
+            Object.keys(this.dados_comp).forEach(key => {
+              //this.indicadores[key].help = 'Para visualizar as interpretações é preciso selecionar pelo menos 1 safra para geração de indicadores.';
+              data.push({
+              'ORDEM': count,
+              'INFORMAÇÃO': this.dados_comp[key].text,
+              'UNIDADE': this.dados_comp[key].unidade,
+              'VALOR': this.formatN(this[key])
+              })
+              count++;
+            });
+
+            let ws = XLSX.utils.json_to_sheet(data);
+            let wb = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(wb, ws, 'Registro ' + (this.m_mes!=null?'mensal '+this.m_mes+'-':'anual de ') + this.m_ano);
+
+            try {
+              XLSX.writeFileAsync(filename, wb, (err)=>{
+                if(err) remote.dialog.showErrorBox('Erro ao gravar o arquivo!', 'Não foi possível criar o arquivo no local.');
+                else remote.dialog.showMessageBox({type:'info', title:'Arquivo XLSX criado com sucesso!', message: 'O arquivo XLSX foi salvo no local escolhido com sucesso!'});
+              });
+            }
+            catch (e){
+              remote.dialog.showErrorBox('Erro ao gravar o arquivo!', 'Não foi possível criar o arquivo no local.');
+            }
+
+          });
+        }
+
+      //============================================================= PDF
+
+      else if(type==1){
+        let data = `<style>
+        table {
+          border-collapse: collapse;
+          width:100%;
+        }
+
+        table, th, td {
+          border: 1px solid black;
+        }
+        th,thead {
+          text-align:center;
+          height:35px;
+        }
+        td {
+          height:26px;
+        }
+        </style>`;
+
+        data += `<body style="font-family: Arial, Helvetica, sans-serif; margin:20px; line-height:15px;">
+          <h1>Fazenda ${this.fazenda_ident}</h1>
+          <b>Registro ${this.m_mes!=null?'mensal '+this.m_mes+'/':'anual de '}${ this.m_ano } </b><br /><br />
+          <table>
+            <thead>
+              <tr><th>#</th><th>INFORMAÇÃO</th><th>UNIDADE</th><th>VALOR</th></tr>
+            </thead>
+            <tbody>`;
+
+        let count=1;
+        Object.keys(this.dados_comp).forEach(key => {
+          console.log(key);
+          let tmp = this.formatN(this[key])
+          data += `<tr>
+            <td>${count}</td>
+            <td>${this.dados_comp[key].text}</td>
+            <td>${this.dados_comp[key].unidade}</td>
+            <td>${tmp}</td>
+          </tr>`;
+          count++;
+        });
+
+        data += `</tbody></table>
+        </body>`;
+        ipcRenderer.send('print-pdf', data);
+      }
+    },
     showElementos: function(){
       this.view = !this.view
     },
     formatN(vr,minimium=2){
+      if(vr=='-') return '-';
+      if(isNaN(vr)) return parseFloat(parseFloat(0).toFixed(2)).toLocaleString('pt-BR', {maximumFractionDigits: 2, minimumFractionDigits: minimium});
       return parseFloat(vr.toFixed(2)).toLocaleString('pt-BR', {maximumFractionDigits: 2, minimumFractionDigits: minimium});
     }
   },
